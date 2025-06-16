6d641056f679b8a12b98eab62ac10402d68327e5
조설아 <74286330+snowbeauti@users.noreply.github.com>
2025-06-17 07:13:15 +0900

    Update streamlit_voicebot.py

--- streamlit_voicebot.py
+++ streamlit_voicebot.py
@@ -55,7 +55,7 @@ def TTS(response):
 def main():
     # 기본 설정
     st.set_page_config(
-        page_title="음성 비서 프로그램",
+        page_title="설아의 음성 비서 프로그램",
         layout="wide")
 
     # session state 초기화
